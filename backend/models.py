abf399e1b2c87a9fb39b4b293f76223b6cd4e420
aminishere <zainabamin281@gmail.com>
2025-09-30 19:53:01 +0530

    tables relationship developed

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-Base= declarative_base()
+Base = declarative_base()
 
 class User(Base):
-    __tablename__="users"
-    id = Column(Integer,primary_key=True, index= True)
-    email= Column(String, unique=True, index=True)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
     name = Column(String)
-    hash_password =Column(String)
+    hash_password = Column(String)
 
+class File(Base):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    text = Column(String)
+    url = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
