ff4897386cc75566cbcd6e1ad88bd26653714166
aminishere <zainabamin281@gmail.com>
2025-10-01 14:19:27 +0530

    client for v2

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+   <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-500">Tailwind is working!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
