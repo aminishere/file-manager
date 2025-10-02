0497bd48acc3d1d8c44607ada4b77863112a6887
aminishere <zainabamin281@gmail.com>
2025-10-03 01:25:18 +0530

    render postgres url

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DB_URL ="postgresql://amin:amin123@localhost/mydb"
+#DB_URL ="postgresql://amin:amin123@localhost/mydb"
+DB_URL = "postgresql://mydb_u8s8_user:sKPP95VntK8IlzQpTYliYvlBdA7GWiUV@dpg-d3fcss49c44c738c2q8g-a.oregon-postgres.render.com/mydb_u8s8"
 
 engine = create_engine(DB_URL)
 
